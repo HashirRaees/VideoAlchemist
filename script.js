0facc959a3fdaa590a1112d564cde90ee67dc991
HashirRaees <hashirrais13@gmail.com>
2025-08-04 19:42:10 +0500

    added edits section slider

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ gsap.to(".pr, .ae",{
 })
 gsap.to(".marque",{
     transform: "translateX(-100%)",
-    duration: 10,
+    duration: 15,
     ease: "none",
     repeat: -1,
 })
@@ -98,30 +98,54 @@ gsap.from(".abt-cont p",{
 });
 gsap.to(".marque2",{
     transform: "translateX(-100%)",
-    duration: 10,
+    duration: 15,
     ease: "none",
     repeat: -1,
 })
+gsap.from(".edit-head",{
+    opacity:0,
+    duration: .7,
+    y: 20,
+    scrollTrigger:{
+        scroller: "body",
+        trigger: ".edit-head",
+        // markers: true,
+        start: "top 63%"
+    }
+});
 
+// slider script
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+$(document).ready(function(){
+ $('.edit-cont').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+]
+});
 });
